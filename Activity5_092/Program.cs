0b12ddc504640ff499c5f5e0cfb3525ceace96fd
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-11-16 16:22:58 +0700

    Add project files.

---
+++ Activity5_092/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity5_092
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
